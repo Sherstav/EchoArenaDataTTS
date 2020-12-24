7a3967fb57a197497d8f08ad13bd19f130bbd2d6
SuperStav <morrstavrostplz@gmail.com>
2020-12-23 23:32:22 -0800

    added more stuff

---
+++ README.md
@@ -0,0 +1,18 @@
+# EchoArenaDataTTS
+
+## Description
+
+EchoArenaDataTTS is a program that runs in the background while you play Echo VR, and when you join a match reads to you
+
+## Requirements
+
+To use this program, you require:
+
+- Echo VR on PC
+This program only works with Echo VR on PC, meaning running on a Rift of Rift S. It should work with Oculus Link on the Quest, although this has not been tested.
+- node.js
+This program runs on node.js and requires it to work. You can download the latest version [here](https://nodejs.org/en/download/).
+
+## Install Guide
+
+Download and extract the latest version of EchoArenaDataTTS into its own folder.
--- index.js
+++ index.js
@@ -1,14 +1,88 @@
 const axios = require("axios");
 const googleTTS = require("google-tts-api"); // here lies the remains of reguire()
 const audic = require("audic");
-const config = require("./api-key.json");
+const fs = require("fs");
 
 //sound = new Audic();
 
+let date = new Date();
+
+function GetLogName()
+{
+    date = new Date();
+    return "log-" + (1+date.getMonth()) +"-"+date.getDate()+"-"+date.getFullYear()+".txt";
+}
+function GetTimeStamp()
+{
+    date = new Date();
+    return "[" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "]";
+}
+
+function LogError(str)
+{
+    console.error(str);
+    fs.appendFile("./logs/"+GetLogName(), GetTimeStamp() + " " + str + "\n", ()=>{});
+}
+
+function Log(str)
+{
+    fs.appendFile("./logs/"+GetLogName(), GetTimeStamp() + " " + str + "\n", ()=>{});
+}
+
+fs.appendFile("./logs/" + GetLogName(), "Program startup"+"\n" , ()=>{
+    Log("Startup");
+});
+
+function PrintError(errorWhile, errorText)
+{
+    LogError("Error while "+errorWhile+": " + errorText);
+    LogError("Please report this to a developer on Discord: shersal#2106  Registered#1266");
+    LogError("If you can't send a direct message, join the Echo VR Discord server and try again.");
+}
+
 let lastMap = "";
+
+if(!fs.existsSync("./api-key.txt"))
+{
+    LogError("No api-key file found. Generating one. You must enter your API Key into the file for the program to run. Instructions can be found in the installation tutorial");
+    fs.appendFile("api-key.txt", " ", (err)=>{
+        PrintError("generating api-key file", err);
+    });
+    process.exit(1);
+}
+const apiKey = fs.readFileSync("./api-key.txt","utf8");
+
+axios.get("http://api.ipstack.com/1.1.1.1?access_key="+apiKey).then((response)=>{
+    let data = response.data;
+
+    if (data.hasOwnProperty("success") && data.success == false)
+    {
+        if (data.error.code == 101)
+        {
+            LogError("Invalid api-key! Check the installation instructions to create a valid api key.");
+        }
+        else if (data.error.code == 104)
+        {
+            LogError("Usage limit reached for this api key.");
+        }
+        else
+        {
+            PrintError("sending api-key check request", data.error.info);
+        }
+        process.exit(1);
+    }
+    else
+    {
+        Log("Success checking api-key.");
+        setInterval(Loop, 1000);
+    }
+}).catch((err)=>{
+    PrintError("sending api-key check request", err);
+})
 
 function Loop()
 {
+
     Main();
 }
 
@@ -16,30 +90,30 @@ function Main()
 {
     // send request
     axios.get("http://127.0.0.1:6721/session").then((response)=>{
-        //console.log(response.data.response);
+        //Log(response.data.response);
         if(response.data.map_name != lastMap)
         {
             lastMap = response.data.map_name;
-            console.log("Just joined");
+            Log("Just joined");
             ProcessInput(response.data);
         }
     }).catch((err)=>{
-        console.error("ERROR: " + err);
+        LogError("ERROR: " + err);
     })
 }
 
 function ProcessInput(data)
 {
     let ip = data.sessionip;
-    console.log("http://api.ipstack.com/" + ip + "?access_key=90ce56843efd551c3298e412c8f9d1d8");
+    Log("http://api.ipstack.com/" + ip + "?access_key=" + apiKey);
 
-    axios.get("http://api.ipstack.com/" + ip + "?access_key=90ce56843efd551c3298e412c8f9d1d8").then((response)=>{
-        let data = response.data; 
-        console.log(data);
+    axios.get("http://api.ipstack.com/" + ip + "?access_key=" + apiKey).then((response)=>{
+        let data = response.data;
+        Log(data);
         let location = {continent: data.continent_name, continent_code: data.continent_code, country: data.country_name, country_code: data.country_code, region: data.region_name, city: data.city};
         GenerateOutput(location);
     }).catch((err)=>{
-        console.error(err);
+        LogError(err);
     });
     
     return ip;
@@ -58,5 +132,3 @@ function GenerateOutput(location)
 
     new audic(url).play();
 }
-
-let interval = setInterval(Loop, 1000);
